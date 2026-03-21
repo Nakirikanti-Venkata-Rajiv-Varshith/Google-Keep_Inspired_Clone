bc4e5d56156e92fe6965eaa50f8b35b2f8f1f272
Nakirikanti-Venkata-Rajiv-Varshith <omenforvar@gmail.com>
2026-03-21 22:05:56 +0530

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/app/', 
+  base: "/Google-Keep_Inspired_Clone/",
   plugins: [react()],
 })
